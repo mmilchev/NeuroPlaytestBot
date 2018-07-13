680904068844b8f0ae5828041deca13d919a34f2
sqbika <sqbika@gmail.com>
2018-07-13 12:22:48 +0200

    Fixed 0 length field message

--- Commands/Playtest/upcoming.js
+++ Commands/Playtest/upcoming.js
@@ -13,12 +13,13 @@ module.exports = class UpcomingCommand extends Command {
     exec(msg, {finished}) {
         this.client.database.PLAYTESTS.findOne({where: {Finished: false },order: [['When', 'ASC']]}).then((ele) => {
             if (ele == null) msg.reply('No upcoming playtest.');
+            var players = ele.Attendees.map((ele2) => this.client.users.get(ele2).username).join('\n');
             msg.reply({
                 embed: msg.client.util.embed()
                     .setTitle(`Info about the upcoming playtest`)
                     .setColor(this.client.config.color)
                     .setFooter({text: `When: ${ele.When.toString()} | In: ${this.client.helper.forHumans(ele.When - new Date())}`})
-                    .addField('Attendees', ele.Attendees.map((ele2) => this.client.users.get(ele2).username).join('\n'))
+                    .addField('Attendees', players.length == 0 ? 'Noone yet.' : players)
                     .addField('Stage', ele.Stage)
             })
         });
