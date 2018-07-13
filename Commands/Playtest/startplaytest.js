f19c506f41da44f2c356d0a9870990f88b75b162
sqbika <sqbika@gmail.com>
2018-07-13 14:52:51 +0200

    Let's see if the rest works now

--- Commands/Playtest/startplaytest.js
+++ Commands/Playtest/startplaytest.js
@@ -67,45 +67,44 @@ ${pairs.map((pair, ind) => `**Pair ${ind+1}**:
 ${this.client.users.get(pair[0]).username}
 ${this.client.users.get(pair[1]).username}`).join('\n')}
 ${alone == undefined ? '' : '\nPairless player: ' + this.client.users.find(alone).username + '\n'}
-Moving players to voicechannel in 10 seconds.`);/*
-        await new Promise(() => setTimeout(() => {
-            pairs.forEach((ele, ind) => {
-                for (var user in ele) {
-                    msg.guild.members.get('user').setVoiceChannel(VC[ind]);
-                }
-            })
-        }, 10000));
-        var time = new Date();
-        msg.channel.send("Stage 0 complete. Entering stage 1. Please start searching according to the pair number in ascending order.");
-        playtest.Phase = 1;
-        await new Promise(() => setTimeout(() => {
-            msg.channel.send("5 minutes remaining of Phase 1.");
-        }, 2100000));
-        await new Promise(() => setTimeout(() => {
-            players = Object.assign([], playtest.Attendees);
-            msg.channel.send("This marks the end of Phase 1. Now entering Phase 2.");
-            playtest.Phase = 2;
-        }, 300000));
-        await new Promise(() => setTimeout(() => {
-            msg.channel.send("5 minutes remaining of Phase 2.");
-        }, 900000));
-        await new Promise(() => setTimeout(() => {
-            msg.channel.send("This marks the end of Phase 2. Please everyone join a common channel to discuss today's playtest.");
-            playtest.Phase = 3;
-        }, 300000));
-        await new Promise(() => setTimeout(() => {
-            msg.channel.send("And that marks today's playtest. Thank you for attending and have a nice day!");
-            playtest.Phase = 4;
-            playtest.Finished = true;
-            playtest.Pairs = JSON.stringify(pairs.map((ele) => ele.map((usr) => this.client.users.get(usr).username)));
-        }, 600000));
-        this.client.database.findOne({
-            where: {
-                id: playtest.id
-            }
-        }).then((res) => {
-            res.update(playtest);
-        })*/
+Moving players to voicechannel in 10 seconds.`);
+		await new Promise(() => setTimeout(() => {
+			pairs.forEach((ele, ind) => {
+				for (var user in ele) {
+					msg.guild.members.get(user).setVoiceChannel(VC[ind]);
+				}
+			});
+		}, 10000));
+		msg.channel.send('Stage 0 complete. Entering stage 1. Please start searching according to the pair number in ascending order.');
+		playtest.Phase = 1;
+		await new Promise(() => setTimeout(() => {
+			msg.channel.send('5 minutes remaining of Phase 1.');
+		}, 2100000));
+		await new Promise(() => setTimeout(() => {
+			players = Object.assign([], playtest.Attendees);
+			msg.channel.send('This marks the end of Phase 1. Now entering Phase 2.');
+			playtest.Phase = 2;
+		}, 300000));
+		await new Promise(() => setTimeout(() => {
+			msg.channel.send('5 minutes remaining of Phase 2.');
+		}, 900000));
+		await new Promise(() => setTimeout(() => {
+			msg.channel.send('This marks the end of Phase 2. Please everyone join a common channel to discuss today\'s playtest.');
+			playtest.Phase = 3;
+		}, 300000));
+		await new Promise(() => setTimeout(() => {
+			msg.channel.send('And that marks today\'s playtest. Thank you for attending and have a nice day!');
+			playtest.Phase = 4;
+			playtest.Finished = true;
+			playtest.Pairs = JSON.stringify(pairs.map((ele) => ele.map((usr) => this.client.users.get(usr).username)));
+		}, 600000));
+		this.client.database.findOne({
+			where: {
+				id: playtest.id
+			}
+		}).then((res) => {
+			res.update(playtest);
+		});
 	}
 
 
