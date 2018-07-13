eda3ed8033b3a9269315eea5800bf255e8d921fa
sqbika <sqbika@gmail.com>
2018-07-13 13:26:25 +0200

    Debug1.8

--- Commands/Playtest/startplaytest.js
+++ Commands/Playtest/startplaytest.js
@@ -117,6 +117,7 @@ Moving players to voicechannel in 10 seconds.`);
                 }
             });
             collector.on('end', (coll, reason) => {
+                console.log("test");
                 if (reason == "ready") msg.reply('Everyone is ready. Initalizing Groups.').then(resolve(true)); //hangs here
             })
         })
