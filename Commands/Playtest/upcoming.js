90d9bb8d666db1784d8bb7453c726a5b142b5694
sqbika <sqbika@gmail.com>
2018-07-13 12:26:47 +0200

    Made embed look nicer

--- Commands/Playtest/upcoming.js
+++ Commands/Playtest/upcoming.js
@@ -16,11 +16,11 @@ module.exports = class UpcomingCommand extends Command {
             var players = ele.Attendees.map((ele2) => this.client.users.get(ele2).username).join('\n');
             msg.reply({
                 embed: msg.client.util.embed()
-                    .setTitle(`Info about the upcoming playtest`)
+                    .setTitle(`Info about the upcoming playtest ID:${ele.id}`)
                     .setColor(this.client.config.color)
                     .setFooter(`When: ${ele.When.toString()} | In: ${this.client.helper.forHumans((ele.When - new Date())/1000)}`)
-                    .addField('Attendees', players.length == 0 ? 'Noone yet.' : players)
-                    .addField('Stage', ele.Stage)
+                    .addField('Attendees', players.length == 0 ? 'Noone yet.' : players, true)
+                    .addField('Stage', ele.Stage, true)
             })
         });
     }
