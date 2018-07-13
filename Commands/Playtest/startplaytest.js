d1896d0821f9c7ede3e9cd1a84b4aa172d2913a5
sqbika <sqbika@gmail.com>
2018-07-13 13:53:19 +0200

    Debug 1.10

--- Commands/Playtest/startplaytest.js
+++ Commands/Playtest/startplaytest.js
@@ -49,7 +49,9 @@ module.exports = class StartPlayTestCommand extends Command {
         /*var ready = await this.readyCheck(msg, playtest);
         console.log("hello?");
         if (ready !== true) return msg.reply('Aborting playtest.\n' + ready);*/
+        console.log("asd-1");
         msg.channel.send("Starting playtest.");
+        console.log("asd");
         var pairs = [];
         for (var i = 0; i < Math.floor(players / 2); i++) {
             var num1 = Math.floor(Math.random() * players);
