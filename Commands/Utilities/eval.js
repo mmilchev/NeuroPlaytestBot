71738e27e79187ddeae52f08c3c54dfd771011ce
sqbika <sqbika@gmail.com>
2018-07-14 14:33:03 +0200

    Implemented Basic Eval

---
+++ Commands/Utilities/eval.js
@@ -0,0 +1,61 @@
+const {	Command } = require('discord-akairo');
+const util = require('util');
+
+class EvalCommand extends Command {
+	constructor() {
+		super('eval', {
+			aliases: ['eval', '%'],
+			ownerOnly: true,
+			protected: true,
+			args: [
+				{
+					id: 'code',
+					match: 'content'
+				}
+			]
+		});
+	}
+    
+	async exec(msg, {code}) {
+
+		const token = this.client.token.split('').join('[^]{0,2}');
+		const rev = this.client.token.split('').reverse().join('[^]{0,2}');
+		const tokenRegex = new RegExp(`${token}|${rev}`, 'g');
+        
+		try {
+			var start = new Date();
+			var ev = eval(code);
+			var took = new Date() - start;
+			var result = ev.replace(tokenRegex, '[TOKEN]');
+			if (typeof result !== 'string') result = util.inspect(result, 0);
+			return msg.reply(`**Code:**
+\`\`|\`
+${code}
+\`\`\`
+**Result:**
+\`\`\`js
+${result}
+\`\`\`
+Took: ${took} ms.
+`);
+		} catch (e) {
+            e = e.toString();
+			const token = this.client.token.split('').join('[^]{0,2}');
+			const rev = this.client.token.split('').reverse().join('[^]{0,2}');
+			const tokenRegex = new RegExp(`${token}|${rev}`, 'g');
+			e = e.replace(tokenRegex, '[TOKEN]');
+			return msg.reply(`**Code:**
+\`\`|\`
+${code}
+\`\`\`
+**Error:**
+\`\`\`js
+${e}
+\`\`\`
+Took: ${took} ms.
+`);
+		}
+	}
+}
+
+
