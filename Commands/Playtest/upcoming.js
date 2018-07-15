226518a0fcd03971b318633b66bfbff09e50233e
sqbika <sqbika@gmail.com>
2018-07-15 20:41:37 +0200

    ASC -> DESC

--- Commands/Playtest/upcoming.js
+++ Commands/Playtest/upcoming.js
@@ -11,7 +11,7 @@ module.exports = class UpcomingCommand extends Command {
 	}
 
 	exec(msg) {
-		this.client.database.PLAYTESTS.findOne({where: {Finished: false },order: [['When', 'ASC']]}).then((ele) => {
+		this.client.database.PLAYTESTS.findOne({where: {Finished: false },order: [['When', 'DESC']]}).then((ele) => {
 			if (ele == null) msg.reply('No upcoming playtest.');
 			var players = ele.Attendees.map((ele2) => this.client.users.get(ele2).username).join('\n');
 			msg.reply({
