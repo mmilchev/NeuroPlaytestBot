bfa0b574aaa1397ba732665d16b76ea7bbbddff5
Sqbika <sqbika@gmail.com>
2018-07-26 00:45:02 +0200

    FIXED

--- Commands/Playtest/startplaytest.js
+++ Commands/Playtest/startplaytest.js
@@ -136,7 +136,7 @@ Moving players to voicechannel in 30 seconds.`);
 			var collector = new ReactionCollector(tocheck, () => true);
 			var waittime = setTimeout(() => collector.stop('timeout'), 300000);
 			collector.on('collect', (react) => {
-				toCollect = this.client.helper.arrayRemove(toCollect, react.users.last.id);
+				toCollect = this.client.helper.arrayRemove(toCollect, react.message.users.last.id);
 				tocheck.edit(`Please react to this message, so you are marked as ready. You have 5 minutes to check in. ${toCollect.map((e) => `<@!${e}>`).join(' ')}`)
 				if (toCollect.length == 0) {
 					collector.stop('ready');
