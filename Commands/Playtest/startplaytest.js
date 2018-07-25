cd3382dd2f7771e4c50983d001063e84de419c92
Sqbika <sqbika@gmail.com>
2018-07-26 00:31:27 +0200

    Fixed?

--- Commands/Playtest/startplaytest.js
+++ Commands/Playtest/startplaytest.js
@@ -133,7 +133,7 @@ Moving players to voicechannel in 30 seconds.`);
 			var tocheck = await msg.channel.send(`Please react to this message, so you are marked as ready. You have 5 minutes to check in. ${playtest.Attendees.map((e) => `<!${e}>`).join(' ')}`);
 			await tocheck.react('🤔');
 			var toCollect = playtest.Attendees;
-			var collector = new ReactionCollector(tocheck, (a, b) => console.log(a,b));
+			var collector = new ReactionCollector(tocheck, () => true);
 			var waittime = setTimeout(() => collector.stop('timeout'), 300000);
 			collector.on('collect', (react) => {
 				tocheck.edit(`Please react to this message, so you are marked as ready. You have 5 minutes to check in. ${toCollect.filter((e) => react.find((g) => g == e) == undefined).map((e) => `<!${e}>`).join(' ')}`)
