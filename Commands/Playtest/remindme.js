32a971966dad3b8876e370399752ae9f7c7a5d38
Sqbika <sqbika@gmail.com>
2018-08-15 12:27:31 +0200

    Redid the remind system

--- Commands/Playtest/remindme.js
+++ Commands/Playtest/remindme.js
@@ -13,7 +13,7 @@ module.exports = class RemindmeCommand extends Command {
 		});
     }
     
-    async exec(msg, {playtestid}) {
+    async exec(msg, {playtestid}) { /*
         if (playtestid == "upcoming") var query = {where: {Finished: false },order: [['When', 'DESC']]};
         else var query = {where: {id: parseInt(playtestid)}}
         var playtest = await msg.client.database.PLAYTESTS.findOne(query);
@@ -23,6 +23,6 @@ module.exports = class RemindmeCommand extends Command {
             What: `Playtest ${playtest.id} is about to begin in 30 minutes!`,
             Who: msg.author.id
         });
-        msg.reply("You are going to be notified in 30 minutes before the playtest.");
+        msg.reply("You are going to be notified in 30 minutes before the playtest.");*/
     }
 }
--- postgresql/models.js
+++ postgresql/models.js
@@ -29,7 +29,8 @@ let PLAYTESTS = database.db.define('playtests', {
 	Started: { type: Sequelize.BOOLEAN },
 	Stage: { type: Sequelize.INTEGER },
 	Channel: { type: Sequelize.BIGINT },
-	Pairs: { type: Sequelize.JSONB }
+	Pairs: { type: Sequelize.JSONB },
+	RemindID: { type: Sequelize.INTEGER }
 });
 
 
