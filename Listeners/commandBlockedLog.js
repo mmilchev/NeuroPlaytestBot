f834c135bd3cacafabed3976adaea806359cbdce
Sqbika <sqbika@gmail.com>
2018-10-03 18:32:59 +0200

    Added Aggressive Config Enforcement

--- Listeners/commandBlockedLog.js
+++ Listeners/commandBlockedLog.js
@@ -10,7 +10,13 @@ module.exports = class CommandHandlerCommandBlockedListener extends Listener {
 	}
 
 	exec(msg, command, reason) {
-        msg.client.channels.get(msg.client.config.channels.blocked).send(`-----------------------------------
+		var chan = msg.client.channels.get(msg.client.config.channels.blocked);
+		if (chan == undefined) {
+			msg.reply("Blocked Log Channel not found. Please change values in the config.json in order to restore functionallity. Stopping Bot.")
+			throw("Blocked Log Channel not found. Please change values in the config.json in order to restore functionallity. Stopping Bot.");
+		}
+		
+		chan.send(`-----------------------------------
 [BLOCK] - Command: ${command.id} was blocked.
     Issed by: \`${msg.author.username}#${msg.author.discriminator} / ${msg.author.id}\`
      On: \`${msg.guild === null ? msg.channel.recipient : msg.guild.name}/${msg.channel.name}\`
